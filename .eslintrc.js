8821766c95792d222c8c705e83ab233ed4d57560
Steel Maxima <steelmaxima21@gmail.com>
2026-03-14 20:00:39 +0100

    feat: create getAllRepositories service

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,6 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     '@typescript-eslint/no-explicit-any': 'warn',
+     "@react/no-unescaped-entities": "off"
   },
 };
--- apps/server/src/module/github_app/github_app.controller.ts
+++ apps/server/src/module/github_app/github_app.controller.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../errors/Apperror';
 import { asyncHandler } from '../../middlewares/asyncHandler';
 import GithubAppService from './github_app.service';
 
@@ -6,6 +7,12 @@ export class GithuAppController {
 
   public installGithubApp = asyncHandler(async (req, res) => {
     const { installation_id, code } = req.query;
+    const {"renderLite-access":token} = req.cookies
+    const userId = req.userId
+    if(!userId){
+      throw new AppError("User id not found",404)
+    }
+
 
     if (!installation_id || !code) {
       return res.status(400).json({
@@ -15,6 +22,7 @@ export class GithuAppController {
 
     const installation = await this.githubService.createInstallation(
       Number(installation_id),
+      userId,
       code as string,
     );
 
@@ -32,4 +40,36 @@ export class GithuAppController {
       </script>
     `);
   });
+
+  public getInstallRepos = asyncHandler(async (req, res) => {
+    const {"renderLite-access":jwt_token} = req.cookies
+    console.log(jwt_token)
+    // const  installation_id  = req.query.installation_id as string
+    // console.log(installation_id)
+
+    //   if(Array.isArray(installation_id)){
+    //       console.log(installation_id[0])
+    //   }else{
+    //     console.log(installation_id)
+    //   }
+    // if (!installation_id) {
+    //   return res.status(400).json({
+    //     message: 'Installation ID is requirzed',
+    //   });
+    // }
+    if(!jwt_token){
+      res.status(401).json({
+        message:"Unathorized access"
+      }
+      )
+    }
+    const repos = await this.githubService.getInstallationRepos(jwt_token);
+    return res.status(200).json({
+      messages: 'Repos fetched successfully ',
+      repositories: repos,
+    });
+  });
+  public getInstallationId = asyncHandler(async (req,res)=>{
+    
+  })
 }
